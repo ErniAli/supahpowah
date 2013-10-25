7e353e62fd93d5af4325dd4e1f7050cb2646ba78
Charles Flood <charlesFlood3@gmail.com>
2013-10-24 18:44:15 -0700

    Added a debugging aid to code tree. Code tree not yet debugged.

--- src/middlelayer/CodeTree.java
+++ src/middlelayer/CodeTree.java
@@ -1,6 +1,7 @@
 package middlelayer;
 
 import frontend.Token;
+import java.util.ArrayList;
 import java.util.Stack;
 
 /**
@@ -9,6 +10,10 @@ import java.util.Stack;
  */
 public class CodeTree
 {
+    
+    private ArrayList<Node> definitions = null;
+    
+    
    private Node root = null;
    private Node currentNode = null;
    private Stack<Node> parentStack = null;
@@ -18,11 +23,14 @@ public class CodeTree
       public Node leftChild = null;
       public Node rightChild = null;
       public String value = null;
+      
+      
    }
 
    public CodeTree()
    {
       parentStack = new Stack();
+      definitions = new ArrayList<>();
    }
 
    public void addToken(Token t)
@@ -34,11 +42,17 @@ public class CodeTree
       }
       else if (t.getType() == Token.TokenType.L_PAREN)
       {
-         if (root == null)
+          if(parentStack.size()==0){
+              root = new Node();
+              currentNode = parentStack.push(root);
+              definitions.add(root);
+          }
+          
+         /*if (root == null)
          {
             root = new Node();
             currentNode = parentStack.push(root);
-         }
+         }*/
          else
          {
             currentNode.leftChild = new Node();
@@ -48,7 +62,10 @@ public class CodeTree
       }
       else if (t.getType() == Token.TokenType.R_PAREN)
       {
+          
          currentNode = parentStack.pop();
+         currentNode.rightChild = new Node();
+         currentNode = currentNode.rightChild;
       }
       else
       {
@@ -56,17 +73,52 @@ public class CodeTree
          currentNode.rightChild = new Node();
          currentNode = currentNode.rightChild;
       }
+      printTreeTest();
    }
 
+   
+   public void printTreeTest(){
+       Node cNode = null;
+       Stack<Node> st = new Stack<>();
+       
+       cNode = definitions.get(0);
+       
+       printNode(cNode);
+       
+       System.out.println("-----------------------------------");
+       
+   }
+   
+   private void printNode(Node n){
+       
+       boolean goLeft = false, goRight = false;
+       
+       System.out.print("Node: "+n.hashCode());
+       if(n.value!=null){System.out.println("\tValue: "+n.value);}
+       if(n.leftChild!=null){
+           System.out.println("  Left Child: "+n.leftChild.hashCode());
+           goLeft = true;
+       }
+       if(n.rightChild!=null){
+           System.out.println("  Right Child: "+n.rightChild.hashCode());
+           goRight = true;
+       }
+       System.out.print("\n\n");
+       
+       if(goRight){printNode(n.rightChild);}
+       if(goLeft){printNode(n.leftChild);}
+   }
+   
+   
    public void printTree()
    {
 
       Node cNode = null;
-      Stack<Node> st = new Stack<Node>();
+      Stack<Node> st = new Stack<>();
 
       System.out.print("(");//has to start with this
-      cNode = this.root;
-      st.push(root);
+      cNode = definitions.get(0);
+      st.push(cNode);
 
       while (!st.empty())
       {
@@ -89,7 +141,7 @@ public class CodeTree
             System.out.println();
             for (int i = 0; i < st.size(); i++)
             {
-               System.out.print("\t");
+               System.out.print("   ");
             }
             System.out.print("(");
             st.push(cNode);
